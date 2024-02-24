504c5e7a3676355a4987baf3bd74988902dd9ef5
SesiTechnosertão <sesitechnosertao@gmail.com>
2024-02-24 12:12:11 -0300

    feat: Audios items

--- src/screens/Audio.tsx
+++ src/screens/Audio.tsx
@@ -3,6 +3,34 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import AudioItem from '../components/AudioItem';
 import SearchBar from '../components/SearchBar';
 
+const audiosExemplo = [
+    {
+        id: 1,
+        title: "Titulo do audio",
+        duration: '03:46'
+    },
+    {
+        id: 2,
+        title: "Batman audio sample",
+        duration: '02:40'
+    },
+    {
+        id: 3,
+        title: "pokemon Intro",
+        duration: '01:26'
+    },
+    {
+        id: 4,
+        title: "Fast and Furious",
+        duration: '02:32'
+    },
+    {
+        id: 5,
+        title: "Happy birthday to U",
+        duration: '03:02'
+    },
+]
+
 function Audio() {
     const insets = useSafeAreaInsets();
     return (
@@ -14,8 +42,9 @@ function Audio() {
             paddingRight: insets.right
         }}>
             <SearchBar />
-            {/* <AudioItem /> */}
-
+            {audiosExemplo.map((item) => (
+                <><AudioItem title={item.title} key={item.id} duration={item.duration} /></>
+            ))}
         </View>
     );
 }
